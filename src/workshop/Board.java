0922a2e386c3eca18d7d4dda9fb01ce6b806eff8
NiharikaVanapari <niharikavanapari23@gmail.com>
2022-05-02 10:02:07 +0530

    UC1 added

--- src/workshop/Board.java
+++ src/workshop/Board.java
@@ -1,11 +1,24 @@
 package workshop;
 
-public class Board {
+class Board
+{
+	   public static void main(String[] args)
+	   {
+	        System.out.println("Welcome to Tic Tac Toe Game");
+	        Board();
+	   }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Tic-Tac-Toe Game");
-
+	   public static void Board()
+	   {
+	    char[] board=new char[8];
+	    for(int i=0;i<=2;i++)
+	    {
+	        for(int j=0;j<=2;j++)
+	        {
+	            System.out.print('_');
+	            System.out.print('|');
+	        }
+	        System.out.println();
+	    }
+	   }
 	}
-
-}
